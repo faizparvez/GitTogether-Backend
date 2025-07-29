968f4ecabd04c5240156420542a15384b9948212
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-07-30 00:14:34 +0530

    created the profile view and edit apis

---
+++ src/models/user.js
@@ -0,0 +1,70 @@
+const { default: mongoose } = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:20
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:20
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+        validate(val){
+            if(!validator.isEmail(val)){
+                throw new Error("Invalid Email: "+val);
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        validate(val){
+            if(!validator.isStrongPassword(val)){
+                throw new Error("Create a stronger password");
+            }
+        }
+    },
+    age:{
+        type:Number,
+        min:18
+    },
+    gender:{
+        type:String,
+        validate(val){
+            if(!["Male","Female","Other"].includes(val)){
+                throw new Error("Invalid Gender");
+            }
+        }
+    },
+    photoURL:{
+        type:String,
+        default:"https://imgs.search.brave.com/A2GFpXOxr1UxULY05bBKWwACH-PeWkdpYNBUB9IIUik/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvNTAwcC8w/Ni8zMi9zb2NpYWwt/bmV0d29yay1kZWZh/dWx0LXByb2ZpbGUt/cGljdHVyZS1hdmF0/YXItaWNvbi12ZWN0/b3ItNTcxMjA2MzIu/anBn",
+        validate(val){
+            if(!validator.isURL(val)){
+                throw new Error("Invalid URL");
+            }   
+        }
+    },
+    about:{
+        type:String,
+        default:"Coding is better with company. Let’s build together!"
+    },
+    skills: {
+        type:[String],
+    }
+},{
+    timestamps:true
+});
+
+const User = mongoose.model("User",userSchema);
+module.exports = {User};
---
+++ src/routes/profile.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const profileRouter = express.Router();
+const { userAuth } = require("../middlewares/auth");
+const { validateEditprofileData } = require("../utils/validation");
+
+// check login => attach profile of user while loginAuth => send back the profile
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    res.send("Loggedin user is : " + user);
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+
+// email & password can't be edited
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try{
+    const valid = validateEditprofileData(req);
+    if(valid){
+      const loggedinuser = req.user;
+      
+      Object.keys(req.body).forEach((key)=>{
+        loggedinuser[key]=req.body[key];
+      });
+
+      await loggedinuser.save();
+      res.json({
+        message:`${loggedinuser.firstName}, your profile is updated successfully !`,
+        data:loggedinuser
+      });
+    }
+    else{
+      throw new Error("Invalid Edit Request");
+    }
+  }
+  catch(err){
+    res.status(404).send("ERROR: "+err.message);
+  }
+});
+
+
+
+module.exports = { profileRouter };
