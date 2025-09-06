9f69ede3419b96c2f0497e6568269776f1211b3a
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-09-06 23:34:58 +0530

    fixedthe cors error issue using middleware

--- src/app.js
+++ src/app.js
@@ -1,32 +1,37 @@
 const express = require("express");
-const {connectDB} = require("./config/database");
+const { connectDB } = require("./config/database");
 const app = express();
+const cors = require("cors");
 
 const cookieParser = require("cookie-parser");
 
-const {authRouter} = require("./routes/auth");
-const {profileRouter} = require("./routes/profile");
-const {requestRouter} = require("./routes/request");
-const {userRouter} = require("./routes/user");
+const { authRouter } = require("./routes/auth");
+const { profileRouter } = require("./routes/profile");
+const { requestRouter } = require("./routes/request");
+const { userRouter } = require("./routes/user");
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // your frontend origin (we are whitelisting our frontend domain name)
+    credentials: true, // we can send cookies even if we are not making https request
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.use("/",authRouter);
-app.use("/",profileRouter);
-app.use("/",requestRouter);
-app.use("/",userRouter);
-
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 connectDB()
- .then(()=>{
+  .then(() => {
     console.log("Database connected successfully");
-    app.listen(3000,()=>{
-        console.log("Server is listening on port 3000...");
-    })
- })
- .catch((err)=>{
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000...");
+    });
+  })
+  .catch((err) => {
     console.error("Database connection can't be established");
- })
+  });
