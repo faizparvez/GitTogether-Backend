4639e3b305940e6d9e7cde969c6918b8307af737
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-10-24 02:08:49 +0530

    additions in user schema

--- src/models/user.js
+++ src/models/user.js
@@ -46,6 +46,13 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+    isPremium: {
+        type: Boolean,
+        default: false,
+    },
+    membershipType: {
+        type: String
+    },
     photoURL:{
         type:String,
         default:"https://imgs.search.brave.com/A2GFpXOxr1UxULY05bBKWwACH-PeWkdpYNBUB9IIUik/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvNTAwcC8w/Ni8zMi9zb2NpYWwt/bmV0d29yay1kZWZh/dWx0LXByb2ZpbGUt/cGljdHVyZS1hdmF0/YXItaWNvbi12ZWN0/b3ItNTcxMjA2MzIu/anBn",
