ebf3323b74ef470d023110c68848841bb44135c1
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-10-25 00:43:24 +0530

    server setup for web socket

--- src/app.js
+++ src/app.js
@@ -1,39 +1,46 @@
-// for .env file to work
-require("dotenv").config();
-
 const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
 const cors = require("cors");
-
 const cookieParser = require("cookie-parser");
+
+const http = require("http");
+
+// for .env file to work
+require("dotenv").config();
+// require("./utils/cronjob");
+
+app.use(
+  cors({
+    origin: ["https://git-together-frontend.vercel.app", "http://localhost:5173"], // your frontend origin (we are whitelisting our frontend domain name)
+    credentials: true, // we can send cookies even if we are not making https request
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
 const { authRouter } = require("./routes/auth");
 const { profileRouter } = require("./routes/profile");
 const { requestRouter } = require("./routes/request");
 const { userRouter } = require("./routes/user");
 const { paymentRouter } = require("./routes/payment");
-
-app.use(
-  cors({
-    origin: ["https://git-together-frontend.vercel.app"], // your frontend origin (we are whitelisting our frontend domain name)
-    credentials: true, // we can send cookies even if we are not making https request
-  })
-);
-
-app.use(express.json());
-app.use(cookieParser());
+const initializeSocket = require("./utils/socket");
+// const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Server is listening on port 3000...");
     });
   })
