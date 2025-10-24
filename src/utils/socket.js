bae843bd3aa12267cb917691adf577afa4678ff6
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-10-25 03:40:58 +0530

    saved the chat in db

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,7 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
 const { Chat } = require("../models/chat");
-const ConnectionRequest = require("../models/connectionRequest");
+const { Request } = require("../models/request.js");
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
@@ -28,34 +28,35 @@ const initializeSocket = (server) => {
     socket.on(
       "sendMessage",
       async ({ firstName, lastName, userId, targetUserId, text }) => {
-    //     // Save messages to the database
-    //     try {
+        try {
           const roomId = getSecretRoomId(userId, targetUserId);
           console.log(firstName + " " + text);
 
-    //       // TODO: Check if userId & targetUserId are friends
+          // Save messages to the database
+          // TODO: Check if userId & targetUserId are friends
 
-    //       let chat = await Chat.findOne({
-    //         participants: { $all: [userId, targetUserId] },
-    //       });
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
 
-    //       if (!chat) {
-    //         chat = new Chat({
-    //           participants: [userId, targetUserId],
-    //           messages: [],
-    //         });
-    //       }
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
 
-    //       chat.messages.push({
-    //         senderId: userId,
-    //         text,
-    //       });
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
 
-    //       await chat.save();
+          await chat.save();
           io.to(roomId).emit("messageReceived", { firstName, lastName, text });
-    //     } catch (err) {
-    //       console.log(err);
-    //     }
+        } 
+        catch (err) {
+          console.log(err);
+        }
       }
     );
 
